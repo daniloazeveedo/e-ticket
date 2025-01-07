bc6611d2be0f4a1f582c756ceabc1869f1725f12
Danilo Azevedo <daniloazeveedo@gmail.com>
2025-01-07 19:19:43 -0300

    Atualizando

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let qtd = parseInt(document.getElementById('qtd').value);
 
-    if(tipo.value == 'pista') {
+    if (tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 
 }
@@ -15,6 +19,28 @@ function comprarPista(qtd) {
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponivel para tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponivel para tipo superior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso');
     }
 }
